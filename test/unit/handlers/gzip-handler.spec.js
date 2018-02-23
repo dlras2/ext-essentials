c1495946e097f323a62d030a89a43580e3606cf9
Daniel Rasmussen <development@dlras.net>
2018-02-22 20:26:50 -0600

    Added GzipHandler unit tests.

---
+++ test/unit/handlers/gzip-handler.spec.js
@@ -0,0 +1,91 @@
+const assert = require('assert');
+
+let GzipHandler;
+
+describe('Unit | GzipHandler', () => {
+  beforeEach(() => {
+    GzipHandler = require('../../../lib/handlers/gzip-handler');
+  });
+
+  it('should accept no options', () => {
+    // Arrange
+    // Act
+    const handler = new GzipHandler();
+    // Assert
+    assert.equal(handler.options, undefined);
+  });
+
+  it('should set options during construction', () => {
+    // Arrange
+    const options = {};
+    // Act
+    const handler = new GzipHandler(options);
+    // Assert
+    assert.equal(handler.options, options);
+  });
+
+  describe('deserialize', () => {
+    it('should return errors while unzipping', done => {
+      // Arrange
+      const handler = new GzipHandler();
+      const buffer = Buffer.from('bad gzip');
+      const expected = /incorrect header check/;
+      // Act
+      handler.deserialize(buffer, (err, result) => {
+        // Assert
+        assert.throws(assert.ifError.bind(this, err), expected);
+        assert.equal(result, null);
+        done();
+      });
+    });
+
+    it('should return errors from bad options', done => {
+      // Arrange
+      const options = { chunkSize: 0 };
+      const handler = new GzipHandler(options);
+      const buffer = Buffer.from(
+        'H4sIAAAAAAAACqtWykjNyclXslIqzy/KSVGqBQDRQQnYEQAAAA==',
+        'base64'
+      );
+      const expected = /ERR_INVALID_OPT_VALUE/;
+      // Act
+      handler.deserialize(buffer, (err, result) => {
+        // Assert
+        assert.throws(assert.ifError.bind(this, err), expected);
+        assert.equal(result, null);
+        done();
+      });
+    });
+  });
+
+  describe('serialize', () => {
+    it('should return errors while zipping', done => {
+      // Arrange
+      const handler = new GzipHandler();
+      const data = { hello: 'world' };
+      const expected = /ERR_INVALID_ARG_TYPE/;
+      // Act
+      handler.serialize(data, (err, result) => {
+        // Assert
+        assert.throws(assert.ifError.bind(this, err), expected);
+        assert.equal(result, null);
+        done();
+      });
+    });
+
+    it('should return errors from bad options', done => {
+      // Arrange
+      const options = { chunkSize: 0 };
+      const handler = new GzipHandler(options);
+      const data = '{"hello":"world"}';
+      const expected = /ERR_INVALID_OPT_VALUE/;
+      // Act
+      handler.serialize(data, (err, result) => {
+        // Assert
+        assert.throws(assert.ifError.bind(this, err), expected);
+        assert.equal(result, null);
+        done();
+      });
+    });
+  });
+});
